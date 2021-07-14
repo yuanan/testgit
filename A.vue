6c09e8c263c4a42626f3cec7b79d64cbe1ae81f1
袁安 <troj.an@qq.com>
2021-07-14 15:09:01 +0800

    1

---
+++ A.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+<script>
+  export default {
+    name: '',
+    data() {
+      return {}
+    },
+    mounted: function () {},
+    methods: {},
+  }
+</script>
+<style lang="less" scoped>
+</style>
